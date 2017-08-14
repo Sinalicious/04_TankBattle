fc1949f597e2bd2070effaa3a874fd097fe8acb0
DESKTOP-5BQEN6A\Sinalicious <h0lyquack.games@gmail.com>
2017-08-14 15:24:25 +0100

    turn movement working

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,11 +1,28 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankMovementComponent.h"
+#include "TankTrack.h"
 
+void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet) {
+	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw) {
 	
-	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
-
+	if (!LeftTrack || !RightTrack) { return; }
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+	// TODO prevent double-speed input due to dual control
 }
+
+void UTankMovementComponent::IntendTurnRight(float Throw) {
+
+	if (!LeftTrack || !RightTrack) { return; }
+	RightTrack->SetThrottle(-Throw);
+	LeftTrack->SetThrottle(Throw);
+	// TODO prevent double-speed input due to dual control
+}
+
 
