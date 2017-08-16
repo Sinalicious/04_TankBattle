d132d22df34fe8b72fd17d251dba4ce6f7380335
DESKTOP-5BQEN6A\Sinalicious <h0lyquack.games@gmail.com>
2017-08-16 19:44:57 +0100

    l180 side mass

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -4,6 +4,23 @@
 #include "BattleTank.h"
 #include "Engine/World.h"
 
+UTankTrack::UTankTrack() {
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) {
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	// UE_LOG(LogTemp, Warning, TEXT("Track ticking"));
+
+	auto SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
+	// Work-out the required acceleration this frame to correct
+	auto CorrectionAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
+	// Calculate and apply sideways for (F = m a)
+	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+	auto CorrectionForce = (TankRoot->GetMass()* CorrectionAcceleration) / 2;
+	TankRoot->AddForce(CorrectionForce);
+}
+
 void UTankTrack::SetThrottle(float Throttle) {
 	
 	// TODO clamp actual throttle value so player can't over-drive
